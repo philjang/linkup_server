2024e16dff336ca4f0190e7fb93de8565112f6e6
philjang <pjang7390@gmail.com>
2022-04-13 15:32:02 -0700

    add discussion model and create superuser

--- linkup_rest_api/discussions_api/models.py
+++ linkup_rest_api/discussions_api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
 class Discussion(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
+    admin = models.IntegerField()
