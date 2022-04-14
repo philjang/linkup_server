4da2f4e2f9d47bb2d1377ae9c18d68dd6cf6ac66
philjang <pjang7390@gmail.com>
2022-04-13 23:18:42 -0700

    add posts model and migration

--- linkup_rest_api/discussions_api/models.py
+++ linkup_rest_api/discussions_api/models.py
@@ -1,9 +1,18 @@
+from tkinter import CASCADE
 from django.db import models
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 # Create your models here.
+# Model schema for discussions_api_discussion table
 class Discussion(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     admin = models.IntegerField()
+
+# Model schema for discussions_api_post table
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    content = models.TextField()
+    time_posted = models.DateTimeField(auto_now_add=True)
