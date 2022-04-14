a9d44d36949eb2c9f4b0b04884f9d72054985a50
philjang <pjang7390@gmail.com>
2022-04-14 13:56:59 -0700

    modify models with __str__ methods

--- linkup_rest_api/discussions_api/models.py
+++ linkup_rest_api/discussions_api/models.py
@@ -10,9 +10,15 @@ class Discussion(models.Model):
     description = models.CharField(max_length=255)
     admin = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 # Model schema for discussions_api_post table
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     time_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
