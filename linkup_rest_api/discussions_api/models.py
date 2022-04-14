aea877be39529462568d4c67612c8ef2421224f8
philjang <pjang7390@gmail.com>
2022-04-14 13:55:49 -0700

    add relations to serializers

--- linkup_rest_api/discussions_api/models.py
+++ linkup_rest_api/discussions_api/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import Group, User
 # Create your models here.
 # Model schema for discussions_api_discussion table
 class Discussion(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='discussions')
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     admin = models.IntegerField()
 
 # Model schema for discussions_api_post table
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     time_posted = models.DateTimeField(auto_now_add=True)
--- linkup_rest_api/discussions_api/serializers.py
+++ linkup_rest_api/discussions_api/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Discussion, Post
 
 # Django uses serializers.ModelSerializer convert sql to JSON
-class DiscussionSerializer(serializers.ModelSerializer): 
-    class Meta:
-        model = Discussion # dictates which model will be used
-        fields = ('id', 'name', 'description', 'admin', 'group')
     
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id','content','time_posted','discussion','user')
+
+class DiscussionSerializer(serializers.ModelSerializer): 
+    posts = PostSerializer(many=True, read_only=True)
+    class Meta:
+        model = Discussion # dictates which model will be used
+        fields = ('id', 'name', 'description', 'admin', 'group')
