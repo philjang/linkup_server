f6ed1df800b8631743d9b491f6504a8e89d0c8d8
philjang <pjang7390@gmail.com>
2022-04-15 01:29:43 -0700

    add user signup serializer

--- linkup_rest_api/membership/serializers.py
+++ linkup_rest_api/membership/serializers.py
@@ -1,15 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Group
+from .serializers import GroupSerializer # to use in UserSerializer
 
 # Django uses serializers.ModelSerializer convert sql to JSON
 # serializers used to read/create/update models
     
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        # can also use fields = __all__, but best practice to be explicit
-        fields = ('id','name','admin')
 
 # defined under GroupSerializer to have access for using as self.posts
 class UserSerializer(serializers.ModelSerializer): 
@@ -18,9 +14,33 @@ class UserSerializer(serializers.ModelSerializer):
     groups = GroupSerializer(many=True, read_only=True)
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'group')
+        fields = ('id', 'username', 'email', 'password') ### do I need password removed?
         # prevents sending password along with data when using views
         extra_kwargs = { 'password': { 'write_only': True, 'min_length': 5 }}
     # method used for model creation
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+class UserRegisterSerializer(serializers.Serializer):
+    # require username, email, password, and password confirmation for sign up
+    username = serializers.CharField(max_length=150, required=True)
+    email = serializers.CharField(max_length=300, required=True)
+    password = serializers.CharField(required=True)
+    password_confirmation = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, data):
+        # check that password and confirmation exist
+        if not data['password'] or not data['password_confirmation']:
+            raise serializers.ValidationError('Please include a password and password confirmation.')
+        # check if the two passwords match
+        if data['password'] != data['password_confirmation']:
+            raise serializers.ValidationError('Please make sure your passwords match.')
+        # return data if both checks pass
+        return data
+
+class GroupSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True, read_only=True)
+    class Meta:
+        model = Group
+        # can also use fields = __all__, but best practice to be explicit
+        fields = ('id','name','admin')
